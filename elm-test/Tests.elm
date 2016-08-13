011c8b89d58313ec2cfb0258413cb370f8d64964
Greg Kubisa <gkubisa@gmail.com>
2016-08-13 13:04:56 +0100

    Remove App.Sample module

--- elm-test/Tests.elm
+++ elm-test/Tests.elm
@@ -1,13 +1,10 @@
 module Tests exposing (testSuite)
 
 import ElmTest exposing (Test, suite)
-import String
-import App.SampleTest as SampleTest
 import App.Widget.MenuTest as MenuTest
 
 testSuite : Test
 testSuite =
   suite "All tests"
-    [ SampleTest.testSuite
-    , MenuTest.testSuite
+    [ MenuTest.testSuite
     ]
