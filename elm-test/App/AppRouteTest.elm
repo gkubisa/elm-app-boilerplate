495d8e5aa7487f130aad6ab5c83bc984f2026733
Greg Kubisa <gkubisa@gmail.com>
2016-08-16 21:59:45 +0100

    Add tests for App.AppRoute.extractOrigin

--- elm-test/App/AppRouteTest.elm
+++ elm-test/App/AppRouteTest.elm
@@ -1,22 +1,39 @@
 module App.AppRouteTest exposing (testSuite)
 
-import Test exposing (Test, describe, test)
 import Expect
-import String
+import Test exposing (Test, describe, test)
 
--- import App.AppRoute exposing (extractOrigin)
+import App.AppRoute exposing (extractOrigin)
 
 testSuite: Test
 testSuite =
   describe "App.AppRoute"
-    -- TODO add some real tests
-    [ test "Addition" <|
-        \() ->
-            Expect.equal (3 + 7) 10
-    , test "String.left" <|
-        \() ->
-            Expect.equal "a" (String.left 1 "abcdefg")
-    -- , test "This test should fail" <|
-    --     \() ->
-    --         Expect.fail "failed as expected!"
+    [ describe "extractOrigin" <|
+        let
+          createTest (description, origin, url) =
+            test description <| \() ->
+              Expect.equal origin (extractOrigin url)
+
+          testCases =
+            [ ( "works for http with no port"
+              , "http://example.com:80"
+              , "http://example.com/some/path"
+              )
+            , ( "works for https with no port"
+              -- TODO port should be 443, waiting for https://github.com/sporto/erl/pull/13
+              -- , "https://example.com:443"
+              , "https://example.com:80"
+              , "https://example.com/some/path"
+              )
+            , ( "works for http with a custom port"
+              , "http://example.com:9999"
+              , "http://example.com:9999/some/path"
+              )
+            , ( "works for https with a custom port"
+              , "https://example.com:9999"
+              , "https://example.com:9999/some/path"
+              )
+            ]
+        in
+          List.map createTest testCases
     ]
