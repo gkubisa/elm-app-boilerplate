1053746b71a19a4f65d6de9ef97a36dc49d6b2b3
Greg Kubisa <gkubisa@gmail.com>
2017-08-28 23:19:29 +0100

    Fix tests

---
+++ tests/App/Widget/MenuTest.elm
@@ -0,0 +1,15 @@
+module App.Widget.MenuTest exposing (..)
+
+import Test exposing (Test, describe, test)
+import Expect
+
+
+suite : Test
+suite =
+    describe "App.Widget.Menu"
+        [ test "placeholder" <|
+            \_ ->
+                Expect.equal 2 2
+
+        -- TODO add some real tests
+        ]
